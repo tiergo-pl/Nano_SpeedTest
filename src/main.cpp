89a613b5608b06bcca4f05a16e539af36328e5c2
Tiergo <tiergo@tiergo.pl>
2022-01-01 15:49:41 +0100

    working 01

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,41 @@
 #include <avr/io.h>
 #include <util/atomic.h>
 #include <util/delay.h>
+#include <avr/builtins.h>
 
 #define MAIN_CLOCK_TICK 10
+#define LED_BUILTIN PB5
+#define SQUARE_OUT PB2
+#define DEBUG_PIN1 PB1
 
-void timer_init(void)
+#define PERIOD_16BIT 20000
+#define PERIOD_CORRECTION_16BIT 0
+#define PERIOD_8BIT 100
+
+volatile uint8_t sysClk8Master, sysClk8, sysClk8Prev;
+volatile uint16_t sysClk16Master, sysClk16, sysClk16Prev;
+
+void timerInit(void)
 {
   OCR2A = 2 * MAIN_CLOCK_TICK - 1;                    // reset Timer2 at 2 * MAIN_CLOCK_TICK
   TCCR2A = 0b00000010;                                //((1<<WGM21) | (0<<WGM20));        // Timer2 CTC mode
   TCCR2B = ((0 << CS22) | (1 << CS21) | (0 << CS20)); // clk/8
   TIMSK2 = (1 << OCIE2A);                             // Timer/Counter Interrupt Mask
 }
-
-
+void portInit()
+{
+  DDRB |= _BV(SQUARE_OUT) | _BV(DEBUG_PIN1) | _BV(LED_BUILTIN);
+  PORTB &= ~(_BV(SQUARE_OUT) | _BV(DEBUG_PIN1) | _BV(LED_BUILTIN));
+}
 
 ISR(TIMER2_COMPA_vect) // TIMER2 interrupt
 {
-  //  PORTC |= _BV(debugPin1);
-  mainClock_us++;
-  //  PORTC &= ~_BV(debugPin1);
+  PORTB |= _BV(DEBUG_PIN1);
+  sysClk8Master++;
+  PORTB &= ~_BV(DEBUG_PIN1);
+  PORTB |= _BV(DEBUG_PIN1);
+  sysClk16Master++;
+  PORTB &= ~_BV(DEBUG_PIN1);
 }
 
 // main starts here =====================================================================================================
@@ -26,7 +43,9 @@ ISR(TIMER2_COMPA_vect) // TIMER2 interrupt
 // main starts here =====================================================================================================
 int main()
 {
-  timer_init();
+  timerInit();
+  portInit();
+
   sei();
 
   // MAIN PROGRAM LOOP =====================================================================================================
@@ -35,9 +54,39 @@ int main()
   // MAIN PROGRAM LOOP
   while (1)
   {
+    
     ATOMIC_BLOCK(ATOMIC_FORCEON)
     {
-      mainClock_us_temp = mainClock_us;
+
+      PORTB |= _BV(DEBUG_PIN1);
+      __builtin_avr_delay_cycles(2);
+      PORTB &= ~_BV(DEBUG_PIN1);
+      sysClk8 = sysClk8Master;
+      sysClk16 = sysClk16Master;
+      PORTB |= _BV(DEBUG_PIN1);
+      PORTB &= ~_BV(DEBUG_PIN1);
     }
+    
+
+    for (uint8_t i = 0; i < 10; i++)
+    {
+      PORTB |= _BV(SQUARE_OUT);
+      PORTB &= ~_BV(SQUARE_OUT);
+      PORTB |= _BV(SQUARE_OUT);
+      PORTB &= ~_BV(SQUARE_OUT);
+      PORTB |= _BV(SQUARE_OUT);
+      PORTB &= ~_BV(SQUARE_OUT);
+      __builtin_avr_delay_cycles(10);
+    }
+
+    int16_t diff = sysClk16 - sysClk16Prev;
+    if (diff > PERIOD_16BIT)
+    {
+      sysClk16Prev = sysClk16 + diff + PERIOD_CORRECTION_16BIT;
+      PINB |= _BV(LED_BUILTIN); // writing to PINx toggles output
+    }
+    
+    PORTB |= _BV(DEBUG_PIN1);
+    PORTB &= ~_BV(DEBUG_PIN1);
   }
 }
